c9e6f6ee76f7723265007523ed5f0fec848bd06a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-01 12:11:35 -0500

    [+] Products > state: Display error messages and load

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -19,11 +19,22 @@ const Products = () => {
     }, [] );
 
     /** Accede al State 'products' del Store */
-    const products = useSelector( state => state .products .products );    // para la propiedad products
+    const 
+        products = useSelector( state => state .products .products ),    // para la propiedad products
+        loading = useSelector( state => state .products .loading ),      // para la propiedad loading
+        error = useSelector( state => state .products .error );          // para la propiedad error
 
     return (
         <Fragment>
             <h2 className="text-center mt-5">Productos</h2>
+            { loading
+                ?   <p className="text-center">Cargando...</p>
+                :   null
+            }
+            { error
+                ?   <p className="alert alert-danger text-center mt-4">Error: No se obtuvo datos de respuesta</p>
+                :   null
+            }
             <table className="table mt-4">
                 <thead className="thead-light">    
                     <tr>
@@ -35,7 +46,9 @@ const Products = () => {
                 </thead>
                 <tbody>
                     { products .length === 0 
-                        ?   <p>No hay productos</p>
+                        ?   <tr>
+                                <td scope="col" colspan="4" className="text-center mt-2">No hay productos</td>
+                            </tr>
                         :   products .map( product => (
                             <Product 
                                 key={ product .id } 

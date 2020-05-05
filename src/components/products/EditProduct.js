fd99458d0f34063ccf1185ef8623fe70c6eaabe5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-05 14:29:18 -0500

    [+] EditProduct: Set the state with the obtained values and allow updating state when editing form fields

--- src/components/products/EditProduct.js
+++ src/components/products/EditProduct.js
@@ -1,22 +1,59 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+
+/** Dependencies */
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 const EditProduct = () => {
 
-    /** Accede al State 'products' del Store */
     const 
-        history = useHistory(),
-        selectedProduct = useSelector( state => state .products .selectedProduct );    // para la propiedad selectedProduct
+    /** Define Component State */
+        [ dataForm, setDataForm ] = useState({
+            name: '',
+            price: 0
+        }),
+    /** Accede al State 'products' del Store */
+        selectedProduct = useSelector( state => state .products .selectedProduct ),    // Dispatch ejecuta las funciones de los actions
+    /** Hook del Router para Redireccion */
+        history = useHistory();      
 
-    /** Valida si hay un producto Seleccionado */
-    console .log( selectedProduct );
-    if( ! selectedProduct ) {
-        history .push( '/' );
-        return null;
+    useEffect( () => {
+
+        console .log( selectedProduct );
+        /** Valida si hay un producto Seleccionado */        
+        if( ! selectedProduct ) {
+            history .push( '/' );
+            return;
+        }
+        /** Update State 'setDataForm' */
+        setDataForm( selectedProduct );     // Actualiza State
+    }, [ selectedProduct ] );
+
+    /** Get form values when they change */
+    const onChangeFormValues = event => {
+        
+        setDataForm({
+            ...dataForm,
+            [ event .target .name ]: ( event .target .name == 'price' )         /** Valida si el campo es price */
+                                        ?   Number( event .target .value )      /** Si se cumple, convierte la entrada en un Number */
+                                        :   event .target .value                /** Si no lo mantiene como viene */
+        });
     }
 
-    const { id, name, price } = selectedProduct;  
+    /** Submit form data */
+    const onSubmitFormData = event => {
+        event .preventDefault();
+
+        // TODO: Valida campos del formulario
+        // TODO: Validar que no hay errores
+        /** Edita producto */
+
+        history .push( '/' );   // Redirecciona a listado de productos
+        
+    }
+
+    /** Destructuring el State del Componente */
+    const { id, name, price } = dataForm;
 
     return (
         <div className="row justify-content-center">
@@ -24,7 +61,9 @@ const EditProduct = () => {
                 <div className="card">
                     <div className="card-body">
                         <h2>Edita Producto</h2>
-                        <form>
+                        <form
+                            onSubmit={ onSubmitFormData }
+                        >
                             <div className="form-group">
                                 <label htmlFor="name">Nombre</label>
                                 <input 
@@ -32,6 +71,8 @@ const EditProduct = () => {
                                     className="form-control" 
                                     id="name" 
                                     placeholder="Nombre del producto" 
+                                    name='name'
+                                    onChange={ onChangeFormValues }
                                     value={ name }
                                 />
                             </div>
@@ -41,11 +82,13 @@ const EditProduct = () => {
                                     type="number" 
                                     className="form-control" 
                                     id="price" 
+                                    name='price'
+                                    onChange={ onChangeFormValues }
                                     value={ price }
                                 />
                             </div>  
                             <button
-                                type="button"
+                                type="submit"
                                 className="btn btn-primary d-block w-100"
                             >Guardar cambios</button>
                         </form>
